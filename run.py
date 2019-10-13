738de008e329a667c84b240381c61396b6ea58d5
Jonathan <jonathan.vengadasalam@gmail.com>
2019-10-13 15:30:09 +0200

    Update run.py

--- run.py
+++ run.py
@@ -40,27 +40,26 @@ def pca():
     xprojected = KernelPCA(n_components=2, kernel="cosine").fit_transform(x)
     plt_2axes_projection(xprojected, y, x_notscaled)
 
-#tsne
 def tsne():
     xprojected_tsne = TSNE(n_components=2, perplexity=30, learning_rate=200, n_iter=5000).fit_transform(x)
     plt_2axes_projection(xprojected_tsne, y, x_not_scaled)
     return xprojected_tsne
 
-#k-means
-km = KMeans(n_clusters=10, init="k-means++", max_iter=500, n_init=20, n_jobs=2)
-km.fit(x_not_scaled)
-plt_matrix_heatmap(confusion_matrix(y, km.labels_), title="Distribution des clusters sur le target")
-plt_clusters_shape(x_notscaled, km.labels_)
-print("adjusted rand : {}\nsilhouette : {}\naccuracy : {}"\
-      .format(adjusted_rand_score(y, km.labels_),silhouette_score(x_not_scaled, km.labels_)))
+def km_clustering():
+    km = KMeans(n_clusters=10, init="k-means++", max_iter=500, n_init=20, n_jobs=2)
+    km.fit(x_notscaled)
+    plt_matrix_heatmap(confusion_matrix(y, km.labels_), title="clusters and tagets distribution")
+    plt_clusters_shape(x_notscaled, km.labels_)
+    print("adjusted rand : {}\nsilhouette : {}"\
+          .format(adjusted_rand_score(y, km.labels_),silhouette_score(x_notscaled, km.labels_)))
 
-#ac
 def ac_clustering():
     ac = AgglomerativeClustering(n_clusters=10, affinity="euclidean", compute_full_tree=True, linkage="ward")
     ac.fit(x_notscaled)
-    matrix_heatmap(confusion_matrix(y, ac.labels_), title="Distribution des clusters sur le target")
+    matrix_heatmap(confusion_matrix(y, ac.labels_), title="clusters and tagets distribution")
     plt_clusters_shape(x_not_scaled, ac.labels_)
-    random_sample(x_not_scaled[ac.labels_ == 3], 40)
+    random_sample(x_notscaled[ac.labels_ == 3], 40)
     plt_2axes_projection(xprojected_tsne, y, y2color=ac.labels_)
     plt.title("Target (number) vs Cluster (color) on TSNE projection", fontdict={"fontsize":"20"})
-    print("adjusted rand : {}\nsilhouette : {}".format(adjusted_rand_score(y, ac.labels_),silhouette_score(x_not_scaled, ac.labels_)))             
+    print("adjusted rand : {}\nsilhouette : {}"\
+          .format(adjusted_rand_score(y, ac.labels_),silhouette_score(x_notscaled, ac.labels_)))             
